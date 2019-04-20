32963fe0a80309523a2a0c05dca5db7e1106f8eb
milliondegree <zhangjiachi1007@gmail.com>
2019-04-19 22:46:52 -0400

    layers

--- block.py
+++ block.py
@@ -5,29 +5,42 @@
 
 import tensorflow as tf
 
-
-
-def conv1d_layer(parent, kernal_size, stride, name, bias = True, relu = False):
+def conv_layer(parent, kernal_size, stride, name, bias = True, relu = False):
     '''
+    conv1d:
     input.shape = [N, sentence_length, channel_number]
     kernel_size.shape = [kernel_length, in_channel, out_channel]
     '''
     with tf.variable_scope(name):
         init_w = init_b = tf.contrib.layers.xavier_initializer(dtype = tf.float32)
-
         weights = tf.get_variable(name = 'weights', shape = kernal_size, dtype = 'float32', initializer = init_w)
-        conv = tf.nn.conv1d(parent, weights, stride, padding = 'SAME')
-
+        conv_func = tf.nn.conv1d if len(parent.shape.as_list)==3 else tf.nn.conv2d
+        conv = conv_func(parent, weights, stride, padding = 'SAME')
         if bias:
             bias = tf.get_variable(name = 'bias', shape = [kernal_size[-1]], dtype = 'float32', initializer = init_b)
             conv_with_bias = tf.nn.bias_add(conv, bias)
         else:
             conv_with_bias = conv
-
         if relu:
             return tf.nn.relu(conv_with_bias)
         else:
             return conv_with_bias
+
+def fc_layer(parent, output_channal, name, bias = True, relu = False):
+    with tf.variable_scope(name):
+        N, W = parent.shape.as_list()
+        init = tf.contrib.layers.xavier_initializer_conv2d(dtype = tf.float32)
+        weights = tf.get_variable(name = 'weights', shape = [W, output_channal], dtype = 'float32', initializer = init)
+        fc = tf.matmul(parent, weights)
+        if bias:
+            bias = tf.get_variable(name = 'bias', shape = [output_channal], dtype = 'float32', initializer = init)
+            fc_with_bias = tf.nn.bias_add(fc, bias)
+        else:
+            fc_with_bias = fc
+        if relu:
+            return tf.nn.relu(fc_with_bias)
+        else:
+            return fc_with_bias
 
 def layer_norm_compute_python(x, epsilon, scale, bias):
     """Layer norm raw computation."""
@@ -41,11 +54,11 @@ def layer_norm(x, filters=None, epsilon=1e-6, scope=None, reuse=None):
     if filters is None:
         filters = x.get_shape()[-1]
     with tf.variable_scope(scope, default_name="layer_norm", values=[x], reuse=reuse):
-        regularizer = tf.contrib.layers.l2_regularizer(scale=3e-7)
+        # regularizer = tf.contrib.layers.l2_regularizer(scale=3e-7)
         scale = tf.get_variable(
-            "layer_norm_scale", [filters], regularizer = regularizer, initializer=tf.ones_initializer())
+            "layer_norm_scale", [filters], initializer=tf.ones_initializer())
         bias = tf.get_variable(
-            "layer_norm_bias", [filters], regularizer = regularizer, initializer=tf.zeros_initializer())
+            "layer_norm_bias", [filters], initializer=tf.zeros_initializer())
         result = layer_norm_compute_python(x, epsilon, scale, bias)
         return result
 
@@ -62,3 +75,11 @@ def depthwise_conv_layer(parent, kernel_size, num_d, num_p, stride, name, bias=T
         if relu:
             output = tf.nn.relu(output)
         return output
+
+def layer_dropout(inputs, residual, dropout):
+    '''
+    randomly using dropout layer
+    '''
+    pred = tf.random_uniform([]) < dropout
+    return tf.cond(pred, lambda: residual, lambda: tf.nn.dropout(inputs, 1.0 - dropout) + residual)
+
