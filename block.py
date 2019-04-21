00d98686f23f61b66a16554998b5fda64c992a62
milliondegree <zhangjiachi1007@gmail.com>
2019-04-21 19:41:35 -0400

    layers

--- block.py
+++ block.py
@@ -28,10 +28,15 @@ def conv_layer(parent, kernal_size, stride, name, bias = True, relu = False):
 
 def fc_layer(parent, output_channal, name, bias = True, relu = False):
     with tf.variable_scope(name):
-        N, W = parent.shape.as_list()
+        shape = parent.shape.as_list()
+        C = shape[-1]
+        if len(shape>=3):
+            parent = tf.reshape(parent, [-1, C])
         init = tf.contrib.layers.xavier_initializer_conv2d(dtype = tf.float32)
-        weights = tf.get_variable(name = 'weights', shape = [W, output_channal], dtype = 'float32', initializer = init)
+        weights = tf.get_variable(name = 'weights', shape = [C, output_channal], dtype = 'float32', initializer = init)
         fc = tf.matmul(parent, weights)
+        if len(shape>=3):
+            fc = tf.reshape(tf, shape)
         if bias:
             bias = tf.get_variable(name = 'bias', shape = [output_channal], dtype = 'float32', initializer = init)
             fc_with_bias = tf.nn.bias_add(fc, bias)
@@ -82,4 +87,37 @@ def layer_dropout(inputs, residual, dropout):
     '''
     pred = tf.random_uniform([]) < dropout
     return tf.cond(pred, lambda: residual, lambda: tf.nn.dropout(inputs, 1.0 - dropout) + residual)
+
+def mask_layer(parent, mask, mod):
+    mask = tf.cast(mask, tf.float32)
+    if mask==None:
+        return parent
+    if mod=='mul':
+        return parent*mask
+    elif mod=='add':
+        return parent - (1-mask)*1e30
+
+def multihead_attention(parent, num_head, size_per_head, name, mask=None, bias=True):
+    with tf.variable_scope(name):
+        N, L, C = parent.shape.as_list()
+        q = fc_layer(parent, num_head*size_per_head, 'q', bias=False)
+        q = tf.reshape(q, (-1, L, num_head, size_per_head))
+        q = tf.transpose(q, [0, 2, 1, 3])
+        v = fc_layer(parent, num_head*size_per_head, 'v', bias=False)
+        v = tf.reshape(v, (-1, L, num_head, size_per_head))
+        v = tf.transpose(v, [0, 2, 1, 3])
+        k = fc_layer(parent, num_head*size_per_head, 'k', bias=False)
+        k = tf.reshape(k, (-1, L, num_head, size_per_head))
+        k = tf.transpose(k, [0, 2, 1, 3])
+        score = tf.matmul(q, k, transpose_b=True)/tf.sqrt((float(size_per_head)))
+        if bias:
+            b = tf.get_variable("bias", [size_per_head], initializer=tf.zero_initializer())
+            score += b
+        score = mask_layer(score, mask, 'add')
+        score = tf.nn.softmax(score)
+        output = tf.matmul(score, v)
+        output = tf.transpose(output, [0, 2, 1, 3])
+        output = tf.reshape(output, [N, L, num_head*size_per_head])
+        return mask_layer(output, mask, 'mul')
+
 
